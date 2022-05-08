97038f9910027a7bee13d06dc525d020c81a3718
LeonardoCriado <70546903+LeonardoCriado@users.noreply.github.com>
2022-05-08 13:29:41 -0300

    Creada clase agencia

---
+++ src/ar/edu/unq/po2/tp5/Agencia.java
@@ -0,0 +1,23 @@
+package ar.edu.unq.po2.tp5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Agencia implements iAgencia{
+	
+	private List<Factura> pagosRegistrados;
+	
+	
+	public Agencia() {
+		super();
+		this.pagosRegistrados = new ArrayList<Factura>();
+	}
+
+
+	@Override
+	public void registrarPago(Factura f) {
+		pagosRegistrados.add(f);		
+	}
+	
+	
+}
